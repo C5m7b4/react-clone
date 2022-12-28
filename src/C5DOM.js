0c83b7aec2de101421baaa311613fde454288c28
C5m7b4 <mbedingfield@dcrpos.com>
2022-12-28 06:10:42 -0600

    add workloop

--- src/C5DOM.js
+++ src/C5DOM.js
@@ -17,6 +17,27 @@ function render(element, container) {
   container.appendChild(dom);
 }
 
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  // while (nextUnitOfWork && !shouldYield) {
+  //   nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+  //   shouldYield = deadline.timeRemaining() < 1;
+  // }
+  while (nextUnitOfWork && !shouldYield) {
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(nextUnitOfwork) {
+  // ToDo
+}
+
 const C5DOM = {
   render,
 };
