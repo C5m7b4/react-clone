5d5fd1055756814d8f4d6b53647a884879d092b0
C5m7b4 <mbedingfield@dcrpos.com>
2022-12-28 07:44:42 -0600

    add event listeners

--- src/index.js
+++ src/index.js
@@ -7,18 +7,48 @@ const root = document.createElement('div');
 root.id = 'root';
 document.body.appendChild(root);
 
-const main = document.createElement('div');
-const child = document.createElement('p');
-child.innerHTML = 'Hello';
-main.appendChild(child);
-root.appendChild(main);
+// const main = document.createElement('div');
+// const child = document.createElement('p');
+// child.innerHTML = 'Hello';
+// main.appendChild(child);
+// root.appendChild(main);
 
-const element = (
-  <div style="background: salmon">
-    <h1>Hello World</h1>
-    <h2 style="text-align:right">from Didact</h2>
-  </div>
-);
+// const handleClick = (e) => {
+//   console.log('e', e);
+//   console.log('i have been pressed');
+// };
+
+const updateValue = (e) => {
+  rerender(e.target.value);
+};
+
 const container = document.getElementById('root');
-console.log('we should be running here');
-C5DOM.render(element, container);
+
+const handleClick = (e) => {
+  console.log('i have been clicked', e);
+};
+
+const rerender = (value) => {
+  const element = (
+    <div>
+      <input onInput={updateValue} value={value} />
+      <h2>Hello {value}</h2>
+      <button onClick={handleClick}>Press Me</button>
+    </div>
+  );
+
+  C5DOM.render(element, container);
+};
+
+rerender('world');
+
+// const element = (
+//   <div style="background: salmon; padding: 5px;">
+//     <h1>Hello World</h1>
+//     <h2 style="text-align:right">from C5</h2>
+//     {/* <button onClick="handleClick()">Press Me</button> */}
+//   </div>
+// );
+// const container = document.getElementById('root');
+// console.log('we should be running here');
+// C5DOM.render(element, container);
